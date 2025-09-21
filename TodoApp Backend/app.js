47722286d0f20d835725d602088379c854f12f95
Hamza Shakoor <144096647+Hamzashakoor315@users.noreply.github.com>
2025-09-21 19:02:52 +0500

    push

--- TodoApp Backend/app.js
+++ TodoApp Backend/app.js
@@ -18,9 +18,10 @@ app.use("/api/todos", require("./routes/todoRoutes"));
 const frontendPath = path.join(__dirname, "dist");  // agar dist folder paste kiya hai
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
